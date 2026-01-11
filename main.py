7613061723423caf243afbc90add90d99157d95e
Izeah Arquillano <izeaharquillano@gmail.com>
2026-01-11 22:08:04 +0800

    did basic init for main

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tkinter as tk
+
+import solver
+import utils
+
+class LeastSquaresApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Least Squares Solver")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LeastSquaresApp(root)
+    root.mainloop()
