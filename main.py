c243e6b316fcfed1ec1e77404b5e10ac56ebd157
Izeah Arquillano <izeaharquillano@gmail.com>
2026-01-12 17:20:45 +0800

    pycode style

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import tkinter as tk
 import solver
 import utils
 
+
 class LeastSquaresApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Least Squares Solver")
+
 
 if __name__ == "__main__":
     root = tk.Tk()
