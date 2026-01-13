097d61e20a50bfa15173d7c973a3507845d479df
Izeah Arquillano <izeaharquillano@gmail.com>
2026-01-13 20:09:57 +0800

    merged uly.py to utils.py

--- uly.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def split_aug_mat(aug_matrix):
-    aug_matrix = np.array(aug_matrix, dtype=float)
-
-    A = aug_matrix[:, :-1]
-    b = aug_matrix[:, -1].reshape(-1, 1)
-
-    return A, b
