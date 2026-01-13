0856689f4ca1b5473df900a03f1b666f86e30be4
Charles Lopez <charlessamuell50@gmail.com>
2026-01-13 19:41:07 +0800

    least square error ffunc

--- solver.py
+++ solver.py
@@ -4,4 +4,12 @@ import numpy as np
 def get_ls_xhat(A, b):
     A = np.array(A, dtype=float)
     b = np.array(b, dtype=float)
+    
     return np.linalg.pinv(A.T @ A) @ A.T @ b
+
+def get_ls_error(A, b, x):
+    
+    prediction = np.dot(A, x)
+    ls_error = np.sum((b - prediction)**2)
+
+    return ls_error
