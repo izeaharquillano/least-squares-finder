e0796e398b02c7ddb4e616649b5a5285ad790239
Charles Lopez <charlessamuell50@gmail.com>
2026-01-13 19:59:51 +0800

    updated the func

--- solver.py
+++ solver.py
@@ -4,12 +4,18 @@ import numpy as np
 def get_ls_xhat(A, b):
     A = np.array(A, dtype=float)
     b = np.array(b, dtype=float)
-    
+
     return np.linalg.pinv(A.T @ A) @ A.T @ b
 
-def get_ls_error(A, b, x):
+def get_ls_error(A, b):
+    A = np.array(A, dtype=float)
+    b = np.array(b, dtype=float)
     
-    prediction = np.dot(A, x)
-    ls_error = np.sum((b - prediction)**2)
+    x_hat = get_ls_xhat(A, b)
+    
+    prediction = A @ x_hat
 
-    return ls_error
+    ls_error = np.linalg.norm(b - prediction)
+    
+    return ls_error 
+   
