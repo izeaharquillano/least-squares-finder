4929eab15bfbe310b55189d047352c0fe3c63a84
Izeah Arquillano <izeaharquillano@gmail.com>
2026-01-13 16:27:02 +0800

    fixed some bugs

--- solver.py
+++ solver.py
@@ -1,4 +1,7 @@
+import numpy as np
+
+
 def get_ls_xhat(A, b):
-  A = np.array(A, dtype=float)
-  b = np.array(b, dtype=float)
-  return np.linalg.inv(A.T @ A) @ A.T @ b
+    A = np.array(A, dtype=float)
+    b = np.array(b, dtype=float)
+    return np.linalg.pinv(A.T @ A) @ A.T @ b
